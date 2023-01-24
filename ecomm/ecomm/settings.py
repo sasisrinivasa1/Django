7e56c53670f3fd28786381a8be96e37c907e4917
sasisrinivasa1 <111584265+sasisrinivasa1@users.noreply.github.com>
2023-01-24 09:43:06 +0000

    Update settings.py

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sasidjango56@gmail.com'
-EMAIL_HOST_PASSWORD = "yourpassword"
+EMAIL_HOST_PASSWORD = "ebghxpfzecrabrwa"
